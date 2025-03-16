5fe8f0daab42d10dee8e9e685972635561373824
Savelii Bondarenko <savabondarenko2016@gmail.com>
2025-03-16 22:46:38 +0200

    1

---
+++ Lab1_4/src/Student.java
@@ -0,0 +1,20 @@
+
+class Student {
+    String lastName;
+    String firstName;
+    String subject;
+    int completedLabs;
+
+    public Student(String lastName, String firstName, String subject, int completedLabs) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.subject = subject;
+        this.completedLabs = completedLabs;
+    }
+
+    @Override
+    public String toString() {
+        return completedLabs + ": " + lastName + " " + firstName + " - "
+                + subject + " Лаби " + completedLabs;
+    }
+}
